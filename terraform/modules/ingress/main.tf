7ac1f3882ab9ae5eac9a07a2eb62101fcff22536
Nick Travers <n.e.travers@gmail.com>
2020-04-05 19:21:07 -0700

    Remove Google Cloud DNS record
    The domain has been transferred to Cloudflare.
    
    Signed-off-by: Nick Travers <n.e.travers@gmail.com>

--- terraform/modules/ingress/main.tf
+++ terraform/modules/ingress/main.tf
@@ -1,15 +1,3 @@
 resource "google_compute_global_address" "ingress" {
   name = "site-ingress-${var.name-suffix}"
 }
-
-data "google_dns_managed_zone" "site" {
-  name = "site"
-}
-
-resource "google_dns_record_set" "ingress" {
-  managed_zone = data.google_dns_managed_zone.site.name
-  name         = var.dns-name
-  type         = "A"
-  rrdatas      = [google_compute_global_address.ingress.address]
-  ttl          = 60
-}
