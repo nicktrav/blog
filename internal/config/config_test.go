2b61c33c0cb2eee827572a388328b203151c126d
Nick Travers <n.e.travers@gmail.com>
2022-07-03 11:33:28 -0700

    *: minimalism (#75)
    Convert back to using boring old HTML and CSS.
    
    Simplify the webserver.
    
    Remove Bazel.
    
    Convert to using fly.io for deployments.

--- internal/config/config_test.go
+++
@@ -1,7 +0,0 @@
-package config
-
-import "testing"
-
-func Test(t *testing.T) {
-	// no-op test
-}
---
+++ pkg/internal/content/manifest.go
@@ -0,0 +1,46 @@
+package content
+
+import (
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Manifest struct {
+	root      string
+	Address   string `yaml:"address"`
+	Templates string `yaml:"templates"`
+	Index     Index  `yaml:"index"`
+	Posts     Posts  `yaml:"posts"`
+	Useful    Posts  `yaml:"useful"`
+}
+
+type Index struct {
+	Template string `yaml:"template"`
+}
+
+type Posts struct {
+	Template string `yaml:"template"`
+	Pages    []Page `yaml:"pages"`
+}
+
+type Metadata map[string]string
+
+type Page struct {
+	URL      string   `yaml:"url"`
+	File     string   `yaml:"file"`
+	Metadata Metadata `yaml:"meta"`
+}
+
+func ParseManifest(path string) (Manifest, error) {
+	m := Manifest{
+		root: filepath.Dir(path),
+	}
+	b, err := os.ReadFile(path)
+	if err != nil {
+		return m, err
+	}
+	err = yaml.Unmarshal(b, &m)
+	return m, err
+}
